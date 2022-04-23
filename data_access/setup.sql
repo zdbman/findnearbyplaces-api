729950170ba62bce41325e33991abbe35a3b1030
Zachary Halperin <zacharykhalperin@gmail.com>
2022-04-23 02:15:56 -0700

    update

---
+++ data_access/setup.sql
@@ -0,0 +1,60 @@
+create schema if not exists yelp;
+
+drop table if exists yelp.place_photo;
+drop table if exists yelp.review_photo;
+drop table if exists yelp.reviews;
+drop table if exists yelp.loc;
+drop table if exists yelp.category;
+drop table if exists yelp.customer;
+
+create table yelp.category
+(
+	id bigserial primary key,
+	name varchar(30)
+);
+
+create table yelp.customer
+(
+	id bigserial primary key,
+	email varchar(256) not null unique,
+	password varchar(8) not null
+);
+
+create table yelp.loc
+(
+	id bigserial primary key unique,
+	name varchar(256) not null,
+	latitude varchar(8) not null,
+	longitude varchar(8) not null,
+	description varchar(512) not null,
+	category_id int references yelp.category(id),
+	customer_id int references yelp.customer(id)
+);
+
+create table yelp.reviews
+(
+	id bigserial primary key,
+	location_id int references yelp.loc(id),
+	customer_id int references yelp.customer(id),
+	text varchar(512) not null,
+	rating varchar(2) not null
+);
+
+drop table if exists yelp.photo;
+create table yelp.photo
+(
+	id bigserial primary key,
+	file varchar(400) not null
+);
+
+create table yelp.place_photo
+(
+	location_id int references yelp.loc(id),
+	photo_id int references yelp.photo(id)
+);
+
+create table yelp.review_photo
+(
+	review_id int references yelp.reviews(id),
+	photo_id int references yelp.photo(id)
+);
